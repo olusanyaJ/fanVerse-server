e5a7373787d3d08cf652d065b2c8546868a7d094
Olusanya Jegede <olusanyajegede1@gmail.com>
2023-11-20 14:40:50 +0000

    knex setup complete

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = 8000;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello World, Welcome to FanVerse!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on PORT ${PORT}`);
+  console.log(`server running at http://localhost:${PORT}`);
 });
